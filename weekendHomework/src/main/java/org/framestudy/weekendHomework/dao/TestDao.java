2aec7efad5ff2e8c230468d54799303a4b88b99d
LQC <LQC@dell.lan>
2017-04-10 11:22:09 +0800

    2017-04-10demo2

--- weekendHomework/src/main/java/org/framestudy/weekendHomework/dao/TestDao.java
+++
@@ -1,8 +0,0 @@
-package org.framestudy.weekendHomework.dao;
-
-import org.framestudy.weekendHomework.beans.Customer;
-import org.framestudy.weekendHomework.beans.Order;
-
-public interface TestDao {
-	public void trade(Customer customer,Order order);
-}
